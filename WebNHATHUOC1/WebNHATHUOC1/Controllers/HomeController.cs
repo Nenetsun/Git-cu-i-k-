bfaf144e14babf45680dace23f1cf19c46a9b80d
Nguyen Minh Triet <gargerath@gmail.com>
2021-12-11 11:21:48 +0700

    Khởi tạo Project, thêm Database, các Index view và HomeController

---
+++ WebNHATHUOC1/WebNHATHUOC1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using WebNHATHUOC1.Models;
+using WebNHATHUOC1.Controllers;
+
+namespace WebNHATHUOC1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private QLNT db = new QLNT();
+      
+        public ActionResult Index()
+        {
+      
+            return View();
+        }
+        public ActionResult Tienich()
+        {
+
+            return View();
+        }
+        public ActionResult Tuvan()
+        {
+
+            return View();
+        }
+        public ActionResult Hotro()
+        {
+
+            return View();
+        }
+        public ActionResult Khachhang()
+        {
+
+            return View();
+        }
+        public ActionResult Kinhnghiem()
+        {
+
+            return View();
+        }
+    }
+}
