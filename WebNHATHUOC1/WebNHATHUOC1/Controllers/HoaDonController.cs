1049e4f3b8f8389bdc9ff464408b31e5b13fea16
HiuHiu <dh51703400@student.stu.edu.vn>
2021-12-12 16:26:19 +0700

    thêm chức năng thêm hóa đơn

--- WebNHATHUOC1/WebNHATHUOC1/Controllers/HoaDonController.cs
+++ WebNHATHUOC1/WebNHATHUOC1/Controllers/HoaDonController.cs
@@ -16,12 +16,12 @@ namespace WebNHATHUOC1.Controllers
         {
             return View(db.HOADONs.ToList());
         }
-        //[HttpGet]
-        //public ActionResult chitietHD(string sohd)
-        //{
-        //    Session["Mahd"] = sohd;
-        //    return View(db.CHITIETHOADONs.Where(x => x.sohd == sohd));
-        //}
+        [HttpGet]
+        public ActionResult chitietHD(string sohd)
+        {
+            Session["Mahd"] = sohd;
+            return View(db.CHITIETHOADONs.Where(x => x.sohd == sohd));
+        }
         //public ActionResult FormDelHD(string sohd)
         //{
         //    Session["Mahd"] = sohd;
@@ -45,7 +45,7 @@ namespace WebNHATHUOC1.Controllers
         //    }
         //    return View("Index");
         //}
-        //#region Xử lý CTHD
+        #region Xử lý CTHD
         //public ActionResult delHD(string sohd, string mathuoc)
         //{
         //    List<Models.CHITIETHOADON> tam = db.CHITIETHOADONs.Where(x => x.sohd == sohd && x.mathuoc == mathuoc).ToList();
@@ -60,41 +60,41 @@ namespace WebNHATHUOC1.Controllers
         //    }
         //    return View("chitietHD");
         //}
-        //public ActionResult FormAddHD()
-        //{
-        //    ViewBag.DSThuoc = db.THUOCs.ToList();
-        //    return View();
-        //}
-        //public ActionResult addCTHD(string sohd, string mathuoc, string soluong)
-        //{
-        //    if(ModelState.IsValid)
-        //    {
-        //        if (db.CHITIETHOADONs.Where(x => x.sohd == sohd && x.mathuoc == mathuoc).ToList().Count < 1)
-        //        {
-        //            Models.CHITIETHOADON a = new CHITIETHOADON();
-        //            a.sohd = sohd;
-        //            a.mathuoc = mathuoc;
-        //            a.donvitinh = db.THUOCs.Find(mathuoc).donvitinh;
-        //            a.dongia = db.THUOCs.Find(mathuoc).dongia;
-        //            a.soluong = Byte.Parse(soluong);
-        //            a.thanhtien = a.dongia * a.soluong;
-        //            db.CHITIETHOADONs.Add(a);
-        //            Models.HOADON hd = db.HOADONs.Find(a.sohd);
-        //            hd.thanhtien = hd.CHITIETHOADONs.Sum(x => x.soluong * x.dongia);
-        //            db.SaveChanges();
-        //            Session["Mahd"] = null;
-        //            return RedirectToAction("Index");
-        //        }
-        //        else
-        //        {
-        //            ModelState.AddModelError("mathuoc", "Thuốc đã có trong hóa đơn!");
-        //            ViewBag.DSThuoc = db.THUOCs.ToList();
-        //            return View("FormAddHD");
-        //        }
-        //    }    
-        //    ViewBag.DSThuoc = db.THUOCs.ToList();
-        //    return View("FormAddHD");
-        //}
+        public ActionResult FormAddHD()
+        {
+            ViewBag.DSThuoc = db.THUOCs.ToList();
+            return View();
+        }
+        public ActionResult addCTHD(string sohd, string mathuoc, string soluong)
+        {
+            if (ModelState.IsValid)
+            {
+                if (db.CHITIETHOADONs.Where(x => x.sohd == sohd && x.mathuoc == mathuoc).ToList().Count < 1)
+                {
+                    Models.CHITIETHOADON a = new CHITIETHOADON();
+                    a.sohd = sohd;
+                    a.mathuoc = mathuoc;
+                    a.donvitinh = db.THUOCs.Find(mathuoc).donvitinh;
+                    a.dongia = db.THUOCs.Find(mathuoc).dongia;
+                    a.soluong = Byte.Parse(soluong);
+                    a.thanhtien = a.dongia * a.soluong;
+                    db.CHITIETHOADONs.Add(a);
+                    Models.HOADON hd = db.HOADONs.Find(a.sohd);
+                    hd.thanhtien = hd.CHITIETHOADONs.Sum(x => x.soluong * x.dongia);
+                    db.SaveChanges();
+                    Session["Mahd"] = null;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("mathuoc", "Thuốc đã có trong hóa đơn!");
+                    ViewBag.DSThuoc = db.THUOCs.ToList();
+                    return View("FormAddHD");
+                }
+            }
+            ViewBag.DSThuoc = db.THUOCs.ToList();
+            return View("FormAddHD");
+        }
         //[HttpGet]
         //public ActionResult FormEditHD(string sohd, string mathuoc)
         //{
@@ -121,128 +121,128 @@ namespace WebNHATHUOC1.Controllers
         //    }
         //    return View("FormEditHD", cthd);
         //}
-        //#endregion
+        #endregion
 
-        //#region Chọn thuốc
-        //public ActionResult FormChonThuoc(string id)
-        //{
-        //    return View(db.THUOCs.ToList());
-        //}
-        //public ActionResult chonThuoc(string id)
-        //{
-        //    List<Models.THUOC> ds = new List<Models.THUOC>();
-        //    Models.THUOC a = db.THUOCs.Find(id);
-        //    if (a != null)
-        //    {
-        //        if (ds.Find(x => x.mathuoc == a.mathuoc) == null)
-        //        {
-        //            ds.Add(a);
-        //        }
-        //    }
-        //    return PartialView(ds);
-        //}
-        //[HttpGet]
-        //public ActionResult delThuoc(string mathuoc)
-        //{
-        //    if(ModelState.IsValid)
-        //    {
-        //        List<Models.CHITIETHOADON> ds = Session["DSCTHD"] as List<Models.CHITIETHOADON>;
-        //        Models.CHITIETHOADON a = ds.Find(x => x.mathuoc == mathuoc);
-        //        if (a != null)
-        //            ds.Remove(a);
-        //        Session["DSCTHD"] = ds;
-        //        return RedirectToAction("FormDonThuoc", ds);
-                
-        //    }
-        //    ViewBag.DSKH = db.KHACHHANGs.ToList();
-        //    return View("FormDonThuoc", Session["DSCTHD"] as List<Models.CHITIETHOADON>);
-        //}
-        //#endregion
+        #region Chọn thuốc
+        public ActionResult FormChonThuoc(string id)
+        {
+            return View(db.THUOCs.ToList());
+        }
+        public ActionResult chonThuoc(string id)
+        {
+            List<Models.THUOC> ds = new List<Models.THUOC>();
+            Models.THUOC a = db.THUOCs.Find(id);
+            if (a != null)
+            {
+                if (ds.Find(x => x.mathuoc == a.mathuoc) == null)
+                {
+                    ds.Add(a);
+                }
+            }
+            return PartialView(ds);
+        }
+        [HttpGet]
+        public ActionResult delThuoc(string mathuoc)
+        {
+            if (ModelState.IsValid)
+            {
+                List<Models.CHITIETHOADON> ds = Session["DSCTHD"] as List<Models.CHITIETHOADON>;
+                Models.CHITIETHOADON a = ds.Find(x => x.mathuoc == mathuoc);
+                if (a != null)
+                    ds.Remove(a);
+                Session["DSCTHD"] = ds;
+                return RedirectToAction("FormDonThuoc", ds);
 
-        //#region Lập đơn thuốc
-        //public ActionResult FormDonThuoc()
-        //{
-        //    List<Models.CHITIETHOADON> ds = Session["DSCTHD"] as List<Models.CHITIETHOADON>;
-        //    ViewBag.DSKH = db.KHACHHANGs.ToList();
-        //    return View(ds);
-        //}
+            }
+            ViewBag.DSKH = db.KHACHHANGs.ToList();
+            return View("FormDonThuoc", Session["DSCTHD"] as List<Models.CHITIETHOADON>);
+        }
+        #endregion
 
-        //[HttpPost]
-        //public ActionResult updateCTHD(string sohd, string mathuoc, string soluong)
-        //{
-        //    List<Models.CHITIETHOADON> ds1 = Session["DSCTHD"] as List<Models.CHITIETHOADON>;
-        //    Models.CHITIETHOADON a = new CHITIETHOADON();
-        //    a.sohd = sohd;
-        //    a.mathuoc = mathuoc;
-        //    a.soluong = Byte.Parse(soluong);
-        //    a.donvitinh = db.THUOCs.Find(a.mathuoc).donvitinh;
-        //    a.dongia = db.THUOCs.Find(a.mathuoc).dongia;
-        //    a.thanhtien = a.dongia * a.soluong;
-        //    Models.CHITIETHOADON b = ds1.Find(x => x.sohd == a.sohd);
-        //    if (ds1.Count == 0)
-        //    {
-        //        ds1.Add(a);
-        //        Session["Mahd"] = a.sohd;
-        //    }
-        //    else if (b != null && b.mathuoc != a.mathuoc)
-        //    {
-        //        ds1.Add(a);
-        //    }
-        //    else if (b != null && b.mathuoc == a.mathuoc)
-        //    {
-        //        ModelState.AddModelError("mathuoc", "Thuốc này đã có trong đơn!");
-        //        return View("FormChonThuoc", db.THUOCs.ToList());
-        //    }
-        //    else if(b == null)
-        //    {
-        //        ModelState.AddModelError("sohd", "Số hóa đơn không đồng bộ!");
-        //        return View("FormChonThuoc", db.THUOCs.ToList());
-        //    }
-        //    ViewBag.DSKH = db.KHACHHANGs.ToList();
-        //    return View("FormDonThuoc", Session["DSCTHD"] as List<Models.CHITIETHOADON>);
-        //}
-        //[HttpPost]
-        //public ActionResult themHoaDon(string ngaylap, string sodt)
-        //{
-        //    if(ModelState.IsValid && ngaylap != "")
-        //    {
-        //        List<Models.CHITIETHOADON> ds = Session["DSCTHD"] as List<Models.CHITIETHOADON>;
-        //        decimal thanhtien = 0;
-        //        if(ds.Count > 0)
-        //        {
-        //            foreach (var item in ds)
-        //            {
-        //                db.CHITIETHOADONs.Add(item);
-        //                thanhtien += (decimal)item.thanhtien;
-        //            }
-        //            Models.HOADON hd = new HOADON();
-        //            Models.NHANVIEN nv = Session["User"] as Models.NHANVIEN;
-        //            hd.makh = sodt;
-        //            hd.manv = nv.manv;
-        //            hd.ngaylap = DateTime.Parse(ngaylap);
-        //            hd.sohd = ds[0].sohd;
-        //            hd.thanhtien = thanhtien;
-        //            db.HOADONs.Add(hd);
-        //            db.SaveChanges();
-        //            Session["DSCTHD"] = new List<Models.CHITIETHOADON>();
-        //            Session["Mahd"] = null;
-        //            return RedirectToAction("Index", "Home");
-        //        }
-        //        else
-        //        {
-        //            ModelState.AddModelError("sohd", "Đơn thuốc rỗng!");
-        //        }    
-        //    }
-        //    else
-        //    {
-        //        ModelState.AddModelError("ngaylap", "Hãy nhập ngày lập!");
-        //        if (sodt == "")
-        //            ModelState.AddModelError("makh", "Hãy nhập mã khách hàng!");
-        //    }
-        //    ViewBag.DSKH = db.KHACHHANGs.ToList();
-        //    return View("FormDonThuoc", Session["DSCTHD"] as List<Models.CHITIETHOADON>);
-        //}
+        #region Lập đơn thuốc
+        public ActionResult FormDonThuoc()
+        {
+            List<Models.CHITIETHOADON> ds = Session["DSCTHD"] as List<Models.CHITIETHOADON>;
+            ViewBag.DSKH = db.KHACHHANGs.ToList();
+            return View(ds);
+        }
 
-        //#endregion
+        [HttpPost]
+        public ActionResult updateCTHD(string sohd, string mathuoc, string soluong)
+        {
+            List<Models.CHITIETHOADON> ds1 = Session["DSCTHD"] as List<Models.CHITIETHOADON>;
+            Models.CHITIETHOADON a = new CHITIETHOADON();
+            a.sohd = sohd;
+            a.mathuoc = mathuoc;
+            a.soluong = Byte.Parse(soluong);
+            a.donvitinh = db.THUOCs.Find(a.mathuoc).donvitinh;
+            a.dongia = db.THUOCs.Find(a.mathuoc).dongia;
+            a.thanhtien = a.dongia * a.soluong;
+            Models.CHITIETHOADON b = ds1.Find(x => x.sohd == a.sohd);
+            if (ds1.Count == 0)
+            {
+                ds1.Add(a);
+                Session["Mahd"] = a.sohd;
+            }
+            else if (b != null && b.mathuoc != a.mathuoc)
+            {
+                ds1.Add(a);
+            }
+            else if (b != null && b.mathuoc == a.mathuoc)
+            {
+                ModelState.AddModelError("mathuoc", "Thuốc này đã có trong đơn!");
+                return View("FormChonThuoc", db.THUOCs.ToList());
+            }
+            else if (b == null)
+            {
+                ModelState.AddModelError("sohd", "Số hóa đơn không đồng bộ!");
+                return View("FormChonThuoc", db.THUOCs.ToList());
+            }
+            ViewBag.DSKH = db.KHACHHANGs.ToList();
+            return View("FormDonThuoc", Session["DSCTHD"] as List<Models.CHITIETHOADON>);
+        }
+        [HttpPost]
+        public ActionResult themHoaDon(string ngaylap, string sodt)
+        {
+            if (ModelState.IsValid && ngaylap != "")
+            {
+                List<Models.CHITIETHOADON> ds = Session["DSCTHD"] as List<Models.CHITIETHOADON>;
+                decimal thanhtien = 0;
+                if (ds.Count > 0)
+                {
+                    foreach (var item in ds)
+                    {
+                        db.CHITIETHOADONs.Add(item);
+                        thanhtien += (decimal)item.thanhtien;
+                    }
+                    Models.HOADON hd = new HOADON();
+                    Models.NHANVIEN nv = Session["User"] as Models.NHANVIEN;
+                    hd.makh = sodt;
+                    hd.manv = nv.manv;
+                    hd.ngaylap = DateTime.Parse(ngaylap);
+                    hd.sohd = ds[0].sohd;
+                    hd.thanhtien = thanhtien;
+                    db.HOADONs.Add(hd);
+                    db.SaveChanges();
+                    Session["DSCTHD"] = new List<Models.CHITIETHOADON>();
+                    Session["Mahd"] = null;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("sohd", "Đơn thuốc rỗng!");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("ngaylap", "Hãy nhập ngày lập!");
+                if (sodt == "")
+                    ModelState.AddModelError("makh", "Hãy nhập mã khách hàng!");
+            }
+            ViewBag.DSKH = db.KHACHHANGs.ToList();
+            return View("FormDonThuoc", Session["DSCTHD"] as List<Models.CHITIETHOADON>);
+        }
+
+        #endregion
     }
 }
